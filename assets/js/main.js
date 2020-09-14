3dbd24fdccd46af6721992b312168b1e14431849
dominicmartincruz <dominicmartincruz@gmail.com>
2020-09-14 18:15:04 +0800

    push to github

---
+++ assets/js/main.js
@@ -0,0 +1,751 @@
+// PARALLAX SCRIPT---------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let controller = new ScrollMagic.Controller();
+    let timeline = new TimelineMax();
+    timeline
+
+
+    .to('#swipeColor', 100, {
+        height: "0%",
+        opacity: "0"
+    })
+
+
+    .to('#letters', 100, {
+        bottom: "100%",
+    })
+
+    .from('#circlePath', 10, {
+        display: 'none'
+    })
+
+
+    .to('#skull', 1, {
+        zIndex: "-1000",
+    })
+
+    .to('#magicMouseCursor', 1, {
+        zIndex: "10000000",
+    })
+
+    .add( function(){ reset() } )
+
+
+    .to('#1', 1, {
+        display: "none",
+    })
+
+    .from('#2', 1, {
+        display: "none",
+    })
+
+    .to('#2', 1, {
+        display: "none",
+    })
+
+    .from('#3', 1, {
+        display: "none",
+    })
+
+    .to('#3', 1, {
+        display: "none",
+    })
+
+    .from('#4', 1, {
+        display: "none",
+    })
+
+    .to('#4', 1, {
+        display: "none",
+    })
+
+    .from('#5', 1, {
+        display: "none",
+    })
+
+    .to('#5', 1, {
+        display: "none",
+    })
+
+    .from('#6', 1, {
+        display: "none",
+    })
+
+    .to('#6', 1, {
+        display: "none",
+    })
+
+    .from('#7', 1, {
+        display: "none",
+    })
+
+    .to('#7', 1, {
+        display: "none",
+    })
+
+    .from('#8', 1, {
+        display: "none",
+    })
+
+    .to('#8', 1, {
+        display: "none",
+    })
+
+    .from('#9', 1, {
+        display: "none",
+    })
+
+    .to('#9', 1, {
+        display: "none",
+    })
+
+    .from('#10', 1, {
+        display: "none",
+    })
+
+    .to('#10', 1, {
+        display: "none",
+    })
+
+    .add( function(){ reset() } )
+
+    .from('#11', 1, {
+        display: "none",
+    })
+
+    .to('#11', 1, {
+        display: "none",
+    })
+
+    .from('#12', 1, {
+        display: "none",
+    })
+
+    .to('#12', 1, {
+        display: "none",
+    })
+
+    .from('#13', 1, {
+        display: "none",
+    })
+
+    .to('#13', 1, {
+        display: "none",
+    })
+
+    .from('#14', 1, {
+        display: "none",
+    })
+
+    .to('#14', 1, {
+        display: "none",
+    })
+
+    .from('#15', 1, {
+        display: "none",
+    })
+
+    .to('#15', 1, {
+        display: "none",
+    })
+
+    .from('#16', 1, {
+        display: "none",
+    })
+
+    .to('#16', 1, {
+        display: "none",
+    })
+
+    .from('#17', 1, {
+        display: "none",
+    })
+
+    .to('#17', 1, {
+        display: "none",
+    })
+
+    .from('#18', 1, {
+        display: "none",
+    })
+
+    .to('#18', 1, {
+        display: "none",
+    })
+
+    .from('#19', 1, {
+        display: "none",
+    })
+
+    .to('#19', 1, {
+        display: "none",
+    })
+
+    .from('#20', 1, {
+        display: "none",
+    })
+
+    .to('#20', 1, {
+        display: "none",
+    })
+
+    .add( function(){ reset() } )
+
+    .from('#21', 1, {
+        display: "none",
+    })
+
+    .to('#21', 1, {
+        display: "none",
+    })
+
+    .from('#22', 1, {
+        display: "none",
+    })
+
+    .to('#22', 1, {
+        display: "none",
+    })
+
+    .from('#23', 1, {
+        display: "none",
+    })
+
+    .to('#23', 1, {
+        display: "none",
+    })
+
+    .from('#24', 1, {
+        display: "none",
+    })
+
+    .to('#24', 1, {
+        display: "none",
+    })
+
+    .from('#25', 1, {
+        display: "none",
+    })
+
+    .to('#25', 1, {
+        display: "none",
+    })
+
+    .from('#26', 1, {
+        display: "none",
+    })
+
+    .to('#26', 1, {
+        display: "none",
+    })
+
+    .from('#27', 1, {
+        display: "none",
+    })
+
+    .to('#27', 1, {
+        display: "none",
+    })
+
+    .from('#28', 1, {
+        display: "none",
+    })
+
+    .to('#28', 1, {
+        display: "none",
+    })
+
+    .from('#29', 1, {
+        display: "none",
+    })
+
+    .to('#29', 1, {
+        display: "none",
+    })
+
+    .from('#30', 1, {
+        display: "none",
+    })
+
+    .to('#30', 1, {
+        display: "none",
+    })
+
+    .add( function(){ reset() } )
+
+    .from('#31', 1, {
+        display: "none",
+    })
+
+    .to('#31', 1, {
+        display: "none",
+    })
+
+    .from('#32', 1, {
+        display: "none",
+    })
+
+    .to('#32', 1, {
+        display: "none",
+    })
+
+    .from('#33', 1, {
+        display: "none",
+    })
+
+    .to('#33', 1, {
+        display: "none",
+    })
+
+    .from('#34', 1, {
+        display: "none",
+    })
+
+    .to('#34', 1, {
+        display: "none",
+    })
+
+    .from('#35', 1, {
+        display: "none",
+    })
+
+    .to('#35', 1, {
+        display: "none",
+    })
+
+    .from('#36', 1, {
+        display: "none",
+    })
+
+    .to('#36', 1, {
+        display: "none",
+    })
+
+    .from('#37', 1, {
+        display: "none",
+    })
+
+    .to('#37', 1, {
+        display: "none",
+    })
+
+    .from('#38', 1, {
+        display: "none",
+    })
+
+    .to('#38', 1, {
+        display: "none",
+    })
+
+    .from('#39', 1, {
+        display: "none",
+    })
+
+    .to('#39', 1, {
+        display: "none",
+    })
+
+    .from('#40', 1, {
+        display: "none",
+    })
+
+    .to('#40', 1, {
+        display: "none",
+    })
+
+    .from('#41', 1, {
+        display: "none",
+    })
+
+    .to('#41', 1, {
+        display: "none",
+    })
+
+    .from('#42', 1, {
+        display: "none",
+    })
+
+    .to('#42', 1, {
+        display: "none",
+    })
+
+    .from('#43', 1, {
+        display: "none",
+    })
+
+    .to('#43', 1, {
+        display: "none",
+    })
+
+    .from('#44', 1, {
+        display: "none",
+    })
+
+    .to('#44', 1, {
+        display: "none",
+    })
+
+    .from('#45', 1, {
+        display: "none",
+    })
+
+    .to('#45', 1, {
+        display: "none",
+    })
+
+    // .to('#faceContainer', 1, {
+    //     filter: "brightness(10%)",
+    // })
+
+    .from('#46', 1, {
+        display: "none",
+    })
+
+    .from('#borderWrapper1', 1, {
+        display: "none",
+    })
+
+    .from('#borderWrapper2', 1, {
+        display: "none",
+    })
+
+    .add( function(){ reset() } )
+
+    .to('#46', 1, {
+        display: "none",
+    })
+
+    .from('#47', 1, {
+        display: "none",
+    })
+
+    .to('#47', 1, {
+        display: "none",
+    })
+
+    .from('#48', 1, {
+        display: "none",
+    })
+
+    .to('#48', 1, {
+        display: "none",
+    })
+
+    .from('#49', 1, {
+        display: "none",
+    })
+
+    .add( function(){ reset() } )
+
+    .to('#49', 50, {
+        opacity: "0",
+        // mixBlendMode: "darken",
+    })
+
+    .add( function(){ showGlitch() } )
+
+    .add( function(){ hideGlitch() } )
+
+    .add( function(){ animate() } )
+
+    .to('#skull', 1, {
+        delay: 30,
+        zIndex: "1000",
+        pointerEvents: "all",
+    })
+
+    .to('#magicMouseCursor', 1, {
+        zIndex: "10000000",
+    })
+
+
+    .from('#helloContainer1', 10, {
+        display: "none",
+    })
+
+    .from('#helloContainer2', 10, {
+        display: "none",
+    })
+
+    .from('#helloContainer3', 10, {
+        display: "none",
+    })
+
+    .from('#helloContainer4', 10, {
+        display: "none",
+    })
+
+    .from('#helloContainer5', 10, {
+        display: "none",
+    })
+
+    .from('#helloContainer6', 10, {
+        display: "none",
+    })
+
+    .from('#iamContainer1', 10, {
+        display: "none",
+    })
+
+    .from('#iamContainer2', 10, {
+        display: "none",
+    })
+
+    .from('#dmbcsvg', 100, {
+        right: "-1000px",
+        display: "none",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#dominic', 10, {
+        display: "none",
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#martinbcruz', 100, {
+        display: "none",
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#helloContainer', 100, {
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#iamContainer', 100, {
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#dominic', 10, {
+        top: "-175px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#martinbcruz', 100, {
+        top: "-175px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#dmbcsvg', 1000, {
+        right: "-200px",
+        // display: "none",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#archisvg', 100, {
+        right: "-1000px",
+        display: "none",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#architect', 100, {
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#licensed', 100, {
+        left: "-1000px",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#architectInfo1', 100, {
+        bottom: "-1500px",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#architectInfo2', 100, {
+        bottom: "-1500px",
+        ease: Power3.easeInOut,
+    })
+
+    .to('#archisvg', 1000, {
+        right: "-200px",
+        // display: "none",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#webdevelopersvg', 1000, {
+        right: "-1000px",
+        display: "none",
+        ease: Power3.easeInOut,
+    })
+
+    
+    .from('#webdevInfo1', 100, {
+        left: "-1500px",
+        ease: Power3.easeInOut,
+    })
+
+    .from('#webdevInfo2', 100, {
+        bottom: "-1500px",
+        ease: Power3.easeInOut,
+    })
+    
+    .to('#borderWrapper1', 100, {
+        opacity: "0",
+    })
+
+    .to('#borderWrapper2', 100, {
+        opacity: "0",
+    })
+
+    .to('#borderWrapper2', 100, {
+        display: "none",
+    })
+
+    .to('#skull', 100, {
+        opacity: "0",
+    })
+
+    .to('#skull', 100, {
+        display: "none",
+    })
+
+
+
+
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: '#section',
+        duration: '1000%',
+        triggerHook: 0,
+        offset: '0'
+    })
+    .setTween(timeline)
+    .setPin('#section')
+    .addTo(controller);
+})
+
+
+const agoratektonProject = document.getElementById('agoratektonProject')
+const tadaoProject = document.getElementById('tadaoProject')
+const produktiboProject = document.getElementById('produktiboProject')
+const contactProject = document.getElementById('contactProject')
+const previous1b = document.getElementById('previous1')
+const previous2b = document.getElementById('previous2')
+const previous3b = document.getElementById('previous3')
+const next1b = document.getElementById('next1')
+const next2b = document.getElementById('next2')
+const next3b = document.getElementById('next3')
+const projectWindow1 = document.getElementById('projectWindow1')
+const projectWindow2 = document.getElementById('projectWindow2')
+const projectWindow3 = document.getElementById('projectWindow3')
+const projectWindow4 = document.getElementById('projectWindow4')
+
+previous1b.style.display = "none"
+previous2b.style.display = "none"
+previous3b.style.display = "none"
+next2b.style.display = "none"
+next3b.style.display = "none"
+
+tadaoProject.style.display="none"
+produktiboProject.style.display="none"
+contactProject.style.display="none"
+
+
+function next1() {
+    console.log('next1')
+    tadaoProject.style.display="flex"
+
+    agoratektonProject.style.left="-100%"
+    tadaoProject.style.left="0%"
+    previous1b.style.display = "flex"
+    next2b.style.display = "flex"
+    next1b.style.display = "none"
+
+    agoratektonProject.style.display="none" 
+}
+
+function next2() {
+    console.log('next2')
+    produktiboProject.style.display="flex"
+
+    tadaoProject.style.left="-100%"
+    produktiboProject.style.left="0%"
+    previous1b.style.display = "none"
+    next2b.style.display = "none"
+    previous2b.style.display = "flex"
+    next3b.style.display = "flex"
+
+    tadaoProject.style.display="none" 
+}
+
+function next3() {
+    console.log('next3')
+    contactProject.style.display="flex"
+
+    produktiboProject.style.left="-100%"
+    contactProject.style.left="0%"
+    previous2b.style.display = "none"
+    next3b.style.display = "none"
+    previous3b.style.display = "flex"
+
+    produktiboProject.style.display="none"
+}
+
+function previous1() {
+    console.log('previous1')
+    agoratektonProject.style.display="flex"
+
+    agoratektonProject.style.left="0%"
+    tadaoProject.style.left="100%"
+    previous1b.style.display = "none"
+    next2b.style.display = "none"
+    next1b.style.display = "flex"
+
+    tadaoProject.style.display="none" 
+}
+
+function previous2() {
+    console.log('previous2')
+    tadaoProject.style.display="flex"
+
+    tadaoProject.style.left="0%"
+    produktiboProject.style.left="100%"
+    previous1b.style.display = "flex"
+    next2b.style.display = "flex"
+    previous2b.style.display = "none"
+    next3b.style.display = "none"
+
+    
+    produktiboProject.style.display="none" 
+}
+
+function previous3() {
+    console.log('previous3')
+    produktiboProject.style.display="flex"
+
+    produktiboProject.style.left="0%"
+    contactProject.style.left="100%"
+    previous3b.style.display = "none"
+    previous2b.style.display = "flex"
+    next3b.style.display = "flex"
+
+    contactProject.style.display="none" 
+}
+
+
+function sendForm(sendThis) {
+    document.getElementById('successSent').style.display="flex"
+    setTimeout(function(){ document.getElementById('successSent').style.display="none" }, 3000);
+    document.getElementById('forms').firstElementChild.nextElementSibling.value = ""
+    document.getElementById('forms').firstElementChild.nextElementSibling.nextElementSibling.value = ""
+    document.getElementById('forms').lastElementChild.value = ""
+}
+
+
+const logo = document.querySelectorAll("#circlePath");
+
+for (let i=0; i < logo.length; i++) {
+    console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+document.getElementById('dSvg').setAttribute = ('transform', "translate(900, 500)")
+
+
+
